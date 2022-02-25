1f5404a69bd05852b993ce5177e17c5af0dbe380
Fernando Francisco Romero <fernandofranciscor333@hotmail.com.ar>
2022-02-24 22:05:07 -0300

    Subte2
    Puertas y movimiento entre estaciones

--- ProyectoFinal-Dreamscape/Assets/Scripts/Subte.cs
+++ ProyectoFinal-Dreamscape/Assets/Scripts/Subte.cs
@@ -7,26 +7,96 @@ public class Subte : MonoBehaviour
     public float speed = 15f;
     private bool canMove = true;
 
+    [SerializeField] private Animator[] RDoors;
+    [SerializeField] private Animator[] LDoors;  
 
-    // Start is called before the first frame update
+    public GameObject Player;
+
+    public float timePass = 0;
+    public float coolDown = 5f; 
+    private bool playerHasCrossed = false; 
+
+    Vector3 forwardDir = Vector3.forward;
+    Vector3 backDir = Vector3.back;
+
+    bool fistStation = false;
+    bool secondStation = false;
+
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        if(canMove)
-            Movement();
+        if(canMove && fistStation == false){
+            Movement(backDir);
+                if(transform.position.z <= 46f){
+                    canMove = false;
+                    if(playerHasCrossed == false)
+                    OpenDoors(LDoors);
+            }
+        }
+
+        if(canMove && fistStation == true){
+            Movement(forwardDir);
+                if(transform.position.z >= 432f){
+                    canMove = false;
+                    if(playerHasCrossed == false)
+                    OpenDoors(LDoors);
+            }
+        }
+
+        if(playerHasCrossed){
+            Player.transform.parent = transform;
+        } else {
+            Player.transform.parent = null;
+        }
+            
+
+
+
+
+        /*if(transform.position.z <= 46f){
+            timePass += Time.deltaTime;
+            canMove = false;
+
+            if(playerHasCrossed == false)
+                OpenDoors(LDoors);
+            
+            if(timePass > coolDown){
+                CloseDoors(LDoors);
+                playerHasCrossed = true;
+            }
+        }*/
     }
 
-    public void Movement(){
-        transform.Translate(speed * Vector3.back * Time.deltaTime);
+    public void Movement(Vector3 dir){
+        transform.Translate(speed * dir * Time.deltaTime);
+    }
+
+    public void OpenDoors(Animator[] doors){
+        foreach(Animator a in doors){
+            a.SetBool("Open", true);
+        }
+    }
+
+    public void CloseDoors(Animator[] doors){
+        foreach(Animator a in doors){
+            a.SetBool("Open", false);
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
-        if(other.tag == "StopTrigger")
-            canMove = false;
+        if(other.tag == "Player" && playerHasCrossed == false){
+            Debug.Log("Entró");
+            playerHasCrossed = true;
+            CloseDoors(LDoors);
+            canMove = true;
+            fistStation = true;
+        }   
     }
 }
